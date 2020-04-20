22e210abb36eec29c21718e8d8f3a31cbc815183
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-04-20 23:04:32 +0530

    [Neha]1.1 Red - should return total car when add one apple

---
+++ src/test/java/com/thoughtworks/assignmen/ShoppingCartTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.assignmen;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    public void givenShoppingCart_whenAddOneItem_ThenShouldReturnTotalCart() {
+        ShoppingCart shoppingCart = new ShoppingCart("apple",1,0.99);
+        int cartTotal = shoppingCart.calculateCartTotal();
+        Assertions.assertEquals(0.99, cartTotal);
+    }
+}
