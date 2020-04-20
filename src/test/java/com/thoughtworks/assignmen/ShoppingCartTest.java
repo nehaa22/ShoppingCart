25ee91528d421cd3204aae54c8002d776c8e3ec7
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-04-20 23:13:42 +0530

    [Neha]1.1 Refactor - rename method name in Test class

--- src/test/java/com/thoughtworks/assignmen/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/assignmen/ShoppingCartTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Test;
 public class ShoppingCartTest {
 
     @Test
-    public void givenShoppingCart_whenAddOneItem_ThenShouldReturnTotalCart() {
+    public void givenShoppingCart_whenAddOneApple_ThenShouldReturnTotalCart() {
         ShoppingCart shoppingCart = new ShoppingCart("apple",1,0.99);
         double cartTotal = shoppingCart.calculateCartTotal();
         Assertions.assertEquals(0.99, cartTotal);
     }
 
     @Test
-    public void givenShoppingCart_whenAddOneItemWithFiveQuantity_ThenShouldReturnTotalCart() {
+    public void givenShoppingCart_whenAddOneAppleWithFiveQuantity_ThenShouldReturnTotalCart() {
         ShoppingCart shoppingCart = new ShoppingCart("apple",5,0.99);
         double cartTotal = shoppingCart.calculateCartTotal();
         Assertions.assertEquals(4.95, cartTotal);
