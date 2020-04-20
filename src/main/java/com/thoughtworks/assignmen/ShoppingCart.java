d704c3858f8a540e5a055f0ca0637f759d28ce21
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-04-20 23:17:28 +0530

    [Neha]1.1 Refactor - rename method name as calculateTotalCart, productName and assign result to totalCart

--- src/main/java/com/thoughtworks/assignmen/ShoppingCart.java
+++ src/main/java/com/thoughtworks/assignmen/ShoppingCart.java
@@ -1,17 +1,21 @@
 package com.thoughtworks.assignmen;
 
 public class ShoppingCart {
-    String proiductName;
+
+    double totalCart;
+
+    String productName;
     int quantity;
     double price;
 
     public ShoppingCart(String proiductName, int quantity, double price) {
-        this.proiductName = proiductName;
+        this.productName = proiductName;
         this.quantity = quantity;
         this.price = price;
     }
 
-    public double calculateCartTotal() {
-        return quantity * price;
+    public double calculateTotalCart() {
+        totalCart = quantity * price;
+        return totalCart;
     }
 }
--- src/test/java/com/thoughtworks/assignmen/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/assignmen/ShoppingCartTest.java
@@ -8,14 +8,14 @@ public class ShoppingCartTest {
     @Test
     public void givenShoppingCart_whenAddOneApple_ThenShouldReturnTotalCart() {
         ShoppingCart shoppingCart = new ShoppingCart("apple",1,0.99);
-        double cartTotal = shoppingCart.calculateCartTotal();
+        double cartTotal = shoppingCart.calculateTotalCart();
         Assertions.assertEquals(0.99, cartTotal);
     }
 
     @Test
     public void givenShoppingCart_whenAddOneAppleWithFiveQuantity_ThenShouldReturnTotalCart() {
         ShoppingCart shoppingCart = new ShoppingCart("apple",5,0.99);
-        double cartTotal = shoppingCart.calculateCartTotal();
+        double cartTotal = shoppingCart.calculateTotalCart();
         Assertions.assertEquals(4.95, cartTotal);
     }
 }
