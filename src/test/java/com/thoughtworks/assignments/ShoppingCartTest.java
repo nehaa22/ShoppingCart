fba6acc9509685eaccad5ae8821ca91f13354148
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-04-27 13:38:07 +0530

    1.5 Red . should apply extra 10% order

--- src/test/java/com/thoughtworks/assignments/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/assignments/ShoppingCartTest.java
@@ -62,5 +62,31 @@ public class ShoppingCartTest {
         Assertions.assertEquals(9.12, shoppingCart.getTotalCartWithTax());
         Assertions.assertEquals("ShoppingCart{cartItems=[CartItem{product=Product{name='Apple', price=0.99}, quantity=3}, CartItem{product=Product{name='Mask', price=1.99}, quantity=3}]}", shoppingCart.toString());
     }
-}
 
+    @Test
+    public void givenShoppingCart_whenAddThreeMaskAndFiveAppleWithOffer_ThenShouldReturnTotalCartOfAppleAndMaskIncludingTax() {
+        Product mask = new Product("Mask", 1.99);
+        Product apple = new Product("Apple", 0.99);
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addCart(apple, 5, "Get3For2");
+        shoppingCart.addCart(mask, 3);
+        Assertions.assertEquals(0.16, shoppingCart.getSalesTax());
+        Assertions.assertEquals(8.11, shoppingCart.getTotalCartWithTax());
+        Assertions.assertEquals(1.98, shoppingCart.getDiscount());
+    }
+
+    @Test
+    public void givenShoppingCart_whenAddTwoApplesWithExtraOffer_ThenShouldReturnTotalCartOfAppleIncludingTax() throws ProductNotFoundException {
+        Product apple = new Product("Apple", 0.99);
+        Product mask = new Product("Mask", 1.99);
+        ShoppingCart shoppingCart = new TenPercentOffOnCart(
+                new BuyTwoGetThreeOffer(
+                        new ShoppingCart()));
+        shoppingCart.addCart(apple, 10);
+        shoppingCart.addCart(mask, 3);
+        Assertions.assertEquals(0.23, shoppingCart.getSalesTax());
+        Assertions.assertEquals(11.84, shoppingCart.getTotalCartWithTax());
+        Assertions.assertEquals(4.26, shoppingCart.getDiscount());
+        Assertions.assertEquals("ShoppingCart{cartItems=[CartItem{product=Product{name='Apple', price=0.99}, quantity=10}, CartItem{product=Product{name='Mask', price=1.99}, quantity=3}]}", shoppingCart.toString());
+    }
+}
